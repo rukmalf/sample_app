cbe705cedd99f2f00be495f7eac993ff129d61b4
Rukmal Fernando <rukmal@gmail.com>
2013-07-16 20:34:36 +0530

    Committing changes from Chapters 3 and 4

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+require 'rbconfig'
+
 gem 'rails', '3.2.13'
 
 # Bundle edge Rails instead:
@@ -8,6 +10,12 @@ gem 'rails', '3.2.13'
 group :development, :test do
 	gem 'sqlite3', '1.3.5'
 	gem 'rspec-rails', '2.11.0'
+	gem 'guard-rspec', '1.2.1'
+	gem 'wdm', '>=0.1.0' if RbConfig::CONFIG['target_os'] =~ /mswin|mingw/i
+	
+	gem 'guard-spork', '1.2.0'
+	gem 'childprocess', '0.3.6'
+	gem 'spork', '0.9.2'
 end
 
 # Gems used only for assets and not required
@@ -22,6 +30,9 @@ gem 'jquery-rails', '2.0.2'
 
 group :test do
 	gem 'capybara', '1.1.2'
+	gem 'rb-fchange', '0.0.5'
+	gem 'rb-notifu', '0.0.4'
+	gem 'win32console', '1.3.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+ 
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
-	it "should have the right title" do
+	it "should have the base title" do
 		visit '/static_pages/home'
 		page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Home")
+			:text => "#{base_title}")
+	end
+	it "should not have a custom page title" do
+		visit '/static_pages/home'
+		page.should_not have_selector('title', :text=> "| Home")
 	end
   end
   
@@ -21,7 +28,7 @@ describe "Static Pages" do
 	it "should have the right title" do
 		visit '/static_pages/help'
 		page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Help")
+			:text => "#{base_title} | Help")
 	end
   end
   
@@ -34,6 +41,6 @@ describe "Static Pages" do
   it "should have the right title" do
 		visit '/static_pages/about'
 		page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | About")
+			:text => "#{base_title} | About")
 	end
 end
