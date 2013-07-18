241e43757711efb1b1446acdd5aca7f36f885498
Rukmal Fernando <rukmal@gmail.com>
2013-07-18 23:21:29 +0530

    Add named routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+  #match '/home',    to: 'static_pages#home'
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+ 
+  # rails creates these for us
+  #about_path   => '/about'
+  #about_url    => 'http://localhost:3000/about'
+  #help_path    => '/help'
+  #help_url     => 'http://localhost:3000/help'
+  #contact_path => '/contact'
+  #contact_url  => 'http://localhost:3000/contact'
   
-  get "static_pages/about"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
