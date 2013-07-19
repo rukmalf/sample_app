232397fd6ed6ac4462c4350ea756fbcc1b731d5e
Rukmal Fernando <rukmal@gmail.com>
2013-07-19 22:51:06 +0530

    Finish layout and routes, add Users and Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   #match '/home',    to: 'static_pages#home'
   root to: 'static_pages#home'
+  
+  match '/signup',  to: 'users#new'
+  
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
