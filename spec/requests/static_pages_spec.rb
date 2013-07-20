ce1b2eef52e956e21f4f458e3136368e06472b0c
Rukmal Fernando <rukmal@gmail.com>
2013-07-20 16:55:45 +0530

    Complete exercise 2 of Rail tutorial, chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,6 +17,25 @@ describe "Static Pages" do
 	it { should_not have_selector('title', text: "| Home") }
   end
   
+  it "should have the right links on the layout" do
+    visit root_path
+	click_link "About"
+	page.should have_selector 'title', text: full_title('About Us')
+	
+	click_link "Help"
+	page.should have_selector 'title', text: full_title('Help')
+	
+	click_link "Contact"
+	page.should have_selector 'title', text: full_title('Contact')
+	
+	click_link "Home"
+	click_link "Sign up now!"
+	page.should have_selector 'title', text: full_title('Sign up')
+	
+	click_link "sample app"
+	page.should_not have_selector 'title', text: '| Home'
+  end
+  
   describe "Help page" do
     before { visit help_path }
 	let(:heading)    { 'Help' }
