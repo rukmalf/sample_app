fd05d543bd900d3a75908ab6d300885f393c3f85
Rukmal Fernando <rukmal@gmail.com>
2013-07-25 20:41:45 +0530

    Add User factory and related tests

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Rukmal Fernando"
+	email    "rukmal@gmail.com"
+	password "foobar"
+	password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,16 @@ describe "UserPages" do
   describe "signup page" do
 	before { visit signup_path }
 	
-	it { should have_selector('h1',    text: 'Sign up') }
+	it { should have_content('Sign up') }
 	it { should have_selector('title', text: full_title('Sign up')) }
+  end
+  
+  describe "profile page" do
+    let (:user) { FactoryGirl.create(:user) }
+	
+	before { visit user_path(user) }
+	
+	it { should have_content(user.name) }
+	it { should have_selector('title', text: full_title(user.name)) }
   end
 end
