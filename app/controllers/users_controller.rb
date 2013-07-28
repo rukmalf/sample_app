131599d4802e4e8d1314ce7c9fa0ab662b88b3d8
Rukmal Fernando <rukmal@gmail.com>
2013-07-28 20:30:20 +0530

    Complete signup actions, add feedback on errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params[:user])
+	if @user.save
+	  redirect_to @user
+	else
+	  render 'new'
+	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ SampleApp::Application.routes.draw do
 
   root to: 'static_pages#home'
   
-  match '/signup',  to: 'users#new'            , via: 'get'
+  #match '/signup',  to: 'users#new'            , via: 'get'
+  match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'    , via: 'get'
   match '/about',   to: 'static_pages#about'   , via: 'get'
   match '/contact', to: 'static_pages#contact' , via: 'get'
